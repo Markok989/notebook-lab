8e30238ecc67e82bd660cc0d72858f9dfd5d5f48
Marko <marko.k.89@gmail.com>
2017-12-28 12:02:12 +0100

    course list is being added to store. need to display

--- database/teacherDb.js
+++ database/teacherDb.js
@@ -26,11 +26,11 @@ function saveNewCourse(data) {
 }
 
 
-// module export for getCoursesByTeacher with parameter data,
+// function getCoursesByTeacher with parameter data,
 // log string: 'DBQUERY: saveNewCourse.',
 // queryStr has value of string: 'SELECT * FROM courses WHERE teacher_id = $1',
 // return db.query with parameters queryStr and data
-module.exports.getCoursesByTeacher = (data) => {
+function getCoursesByTeacher(data) {
     console.log('DBQUERY: saveNewCourse.');
     let queryStr = 'SELECT * FROM courses WHERE teacher_id = $1';
     return db.query(queryStr, data);
@@ -39,6 +39,12 @@ module.exports.getCoursesByTeacher = (data) => {
 // module export for saveNewCourse with value saveNewCourse
 module.exports.saveNewCourse = saveNewCourse;
 
+// module export for getCoursesByTeacher with value saveNewCourse
+module.exports.getCoursesByTeacher = getCoursesByTeacher;
 
 
-//saveNewCourse([1, 'Biology']);
+
+ //saveNewCourse([1, 'Biology']);
+ // getCoursesByTeacher([1]).then((results) => {
+ //     console.log(results.rows);
+ // }).catch(e => console.error(e));
