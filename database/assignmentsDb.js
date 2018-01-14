66321cfdcd6983d8060190a0ea36e64eec1bc8fa
Marko <marko.k.89@gmail.com>
2018-01-14 16:34:52 +0100

    moved assignment queries to separate db file

---
+++ database/assignmentsDb.js
@@ -0,0 +1,231 @@
+var spicedPg = require('spiced-pg');
+var localUrl = '';
+
+/*
+- contition if not process.env.DATABASE_URL
+    - secreat has value of string 'test',
+    - localUrl has value `postgres://qdzpwmxf:4QKHT0tKxYTWp02dCMPk6sCg0RExLYwj@dumbo.db.elephantsql.com:5432/qdzpwmxf`;
+*/
+if (!process.env.DATABASE_URL) {
+
+    // const secrets = require('../secrets.json');
+    const secrets = 'test';
+    localUrl = `postgres://qdzpwmxf:4QKHT0tKxYTWp02dCMPk6sCg0RExLYwj@dumbo.db.elephantsql.com:5432/qdzpwmxf`;
+
+}
+
+// dbUrl has value process.env.DATABASE_URL OR localUrl
+var dbUrl = process.env.DATABASE_URL || localUrl;
+
+// db has value of spicePg with parameter dbUrl
+var db = spicedPg(dbUrl);
+
+
+/********** STUDENT REPORTS *********/
+
+/*
+- function saveNewStudentReport with parameter data
+    - log string 'TEACHER_DB: saveNewStudentReport',
+    - queryStr has value of string `INSERT INTO student_reports (student_id, assignment_id, group_id, abstract_id, question_id, hypothesis_id, variables_id, materials_id, procedures_id, data_id, calculations_id, discussion_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`;
+    - return db query with parameters queryStr and data
+*/
+function saveNewStudentReport(data) {
+
+    console.log('TEACHER_DB: saveNewStudentReport');
+    let queryStr = `INSERT INTO student_reports (student_id, assignment_id, group_id, abstract_id, question_id, hypothesis_id, variables_id, materials_id, procedures_id, data_id, calculations_id, discussion_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`;
+    return db.query(queryStr, data);
+
+}
+
+
+/*
+- function newTitle with parameter data
+    - log string 'TEACHER_DB newAbstract',
+    - queryStr has value of string `INSERT INTO titles (assignment_id, group_id, editable, content) VALUES ($1, $2, $3, $4) RETURNING id`
+    - return db query with parameters queryStr and data
+*/
+function newTitle(data) {
+
+    console.log('TEACHER_DB newAbstract');
+    let queryStr = `INSERT INTO titles (assignment_id, group_id, editable, content) VALUES ($1, $2, $3, $4) RETURNING id`;
+    return db.query(queryStr, data);
+
+}
+
+
+/*
+- function newQuestion with parameter data
+    - log string 'TEACHER_DB newAbstract',
+    - queryStr has value of string `INSERT INTO questions (assignment_id, group_id, editable, content) VALUES ($1, $2, $3, $4) RETURNING id`
+    - return db query with parameters queryStr and data
+*/
+function newQuestion(data) {
+
+    console.log('TEACHER_DB newAbstract');
+    let queryStr = `INSERT INTO questions (assignment_id, group_id, editable, content) VALUES ($1, $2, $3, $4) RETURNING id`;
+    return db.query(queryStr, data);
+
+}
+
+
+/*
+- function newAbstract with parameter data
+    - log string 'TEACHER_DB newAbstract',
+    - queryStr has value of string `INSERT INTO abstracts (assignment_id, group_id, editable, content) VALUES ($1, $2, $3, $4) RETURNING id`
+    - return db query with parameters queryStr and data
+*/
+function newAbstract(data) {
+
+    console.log('TEACHER_DB newAbstract');
+    let queryStr = `INSERT INTO abstracts (assignment_id, group_id, editable, content) VALUES ($1, $2, $3, $4) RETURNING id`;
+    return db.query(queryStr, data);
+
+}
+
+
+/*
+- function newHypothesis with parameter data
+    - log string 'TEACHER_DB newAbstract',
+    - queryStr has value of string `INSERT INTO hypotheses (assignment_id, group_id, editable, content) VALUES ($1, $2, $3, $4) RETURNING id`
+    - return db query with parameters queryStr and data
+*/
+function newHypothesis(data) {
+
+    console.log('TEACHER_DB newAbstract');
+    let queryStr = `INSERT INTO hypotheses (assignment_id, group_id, editable, content) VALUES ($1, $2, $3, $4) RETURNING id`;
+    return db.query(queryStr, data);
+
+}
+
+
+/*
+- function newVariables with parameter data
+    - log string 'TEACHER_DB newAbstract',
+    - queryStr has value of string `INSERT INTO variables (assignment_id, group_id, editable, content) VALUES ($1, $2, $3, $4) RETURNING id`
+    - return db query with parameters queryStr and data
+*/
+function newVariables(data) {
+
+    console.log('TEACHER_DB newAbstract');
+    let queryStr = `INSERT INTO variables (assignment_id, group_id, editable, content) VALUES ($1, $2, $3, $4) RETURNING id`;
+    return db.query(queryStr, data);
+
+}
+
+
+/*
+- function newMaterials with parameter data
+    - log string 'TEACHER_DB newAbstract',
+    - queryStr has value of string `INSERT INTO materials (assignment_id, group_id, editable, content) VALUES ($1, $2, $3, $4) RETURNING id`
+    - return db query with parameters queryStr and data
+*/
+function newMaterials(data) {
+
+    console.log('TEACHER_DB newAbstract');
+    let queryStr = `INSERT INTO materials (assignment_id, group_id, editable, content) VALUES ($1, $2, $3, $4) RETURNING id`;
+    return db.query(queryStr, data);
+
+}
+
+
+/*
+- function newProcedure with parameter data
+    - log string 'TEACHER_DB newAbstract',
+    - queryStr has value of string `INSERT INTO procedures (assignment_id, group_id, editable, content) VALUES ($1, $2, $3, $4) RETURNING id`
+    - return db query with parameters queryStr and data
+*/
+function newProcedure(data) {
+
+    console.log('TEACHER_DB newAbstract');
+    let queryStr = `INSERT INTO procedures (assignment_id, group_id, editable, content) VALUES ($1, $2, $3, $4) RETURNING id`;
+    return db.query(queryStr, data);
+
+}
+
+
+/*
+- function newData with parameter data
+    - log string 'TEACHER_DB newAbstract',
+    - queryStr has value of string `INSERT INTO data (assignment_id, group_id, editable, content) VALUES ($1, $2, $3, $4) RETURNING id`
+    - return db query with parameters queryStr and data
+*/
+function newData(data) {
+
+    console.log('TEACHER_DB newAbstract');
+    let queryStr = `INSERT INTO data (assignment_id, group_id, editable, content) VALUES ($1, $2, $3, $4) RETURNING id`;
+    return db.query(queryStr, data);
+
+}
+
+
+/*
+- function newCalculations with parameter data
+    - log string 'TEACHER_DB newAbstract',
+    - queryStr has value of string `INSERT INTO calculations (assignment_id, group_id, editable, content) VALUES ($1, $2, $3, $4) RETURNING id`
+    - return db query with parameters queryStr and data
+*/
+function newCalculations(data) {
+
+    console.log('TEACHER_DB newAbstract');
+    let queryStr = `INSERT INTO calculations (assignment_id, group_id, editable, content) VALUES ($1, $2, $3, $4) RETURNING id`;
+    return db.query(queryStr, data);
+
+}
+
+
+/*
+- function newDiscussion with parameter data
+    - log string 'TEACHER_DB newAbstract',
+    - queryStr has value of string `INSERT INTO discussions (assignment_id, group_id, editable, content) VALUES ($1, $2, $3, $4) RETURNING id`
+    - return db query with parameters queryStr and data
+*/
+function newDiscussion(data) {
+
+    console.log('TEACHER_DB newAbstract');
+    let queryStr = `INSERT INTO discussions (assignment_id, group_id, editable, content) VALUES ($1, $2, $3, $4) RETURNING id`;
+    return db.query(queryStr, data);
+
+}
+
+
+// TEST:
+
+/*
+- function newAbstract with [42, null, true, "Starting Abstract"]
+- then with word "then" with parameter results we access to function 
+    - log results.rows
+- word "catch" with parameter e
+    - log parameter e
+*/
+newAbstract([42, null, true, "Starting Abstract"]).then(results => {
+    console.log(results.rows);
+}).catch(e => {
+    console.log(e);
+});
+
+//saveNewStudentReport([])
+
+/********** ASSIGNMENTS *********/
+
+/*
+- function saveNewAssignmentTemplate with parameter data
+    - log string 'TEACHER_DB: saveNewAssignmentTemplate, ' and parameter data
+    - queryStr has value of string 'INSERT INTO assignments (section_id, group_lab, name, instructions, due, title, default_title, abstract, default_abstract, question, default_question, hypothesis, default_hypothesis, variables, default_variables, materials, default_materials, procedures, default_procedures, data, default_data, calculations, default_calc, discussion, default_discussion ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25) RETURNING id'
+    - return db query with parameters queryStr and data
+*/
+function saveNewAssignmentTemplate(data) {
+    console.log('TEACHER_DB: saveNewAssignmentTemplate,', data);
+    let queryStr = 'INSERT INTO assignments (section_id, group_lab, name, instructions, due, title, default_title, abstract, default_abstract, question, default_question, hypothesis, default_hypothesis, variables, default_variables, materials, default_materials, procedures, default_procedures, data, default_data, calculations, default_calc, discussion, default_discussion ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25) RETURNING id';
+    return db.query(queryStr, data);
+}
+//Test
+// saveNewAssignmentTemplate([1, false, '3moles', 'instructions', '1999-01-01', 'word', 'word', 'word8', 'word9', 'word9','word11', 'word9','word13', 'word9', 'word15', 'word9', 'word17', 'word9', 'word19', 'word20', 'word21', 'word22', 'word23', 'word24', 'word25'])
+//     .then((results) => {
+//         console.log(results.rows);
+//     }).catch(e => {
+//         console.log(e);
+//     });
+
+// INSERT INTO assignments (section_id, group_lab, name, instructions, due, title, default_title, abstract, default_abstract, question, default_question, hypothesis, default_hypothesis, variables, default_variables, materials, default_materials, procedures, default_procedures, data, default_data, calculations, default_calc, discussion, default_discussion) VALUES (1, false, '3moles', '4no instructions', '1999-01-01', '$6', '$7', '$8', '$9', '10', '11', '12', '13', '14', '$15', '$16', '$17', '$18', '$19', '$20', '21', '22', '$23', '$24', '$25') RETURNING id;
+
+module.exports.saveNewAssignmentTemplate = saveNewAssignmentTemplate;
