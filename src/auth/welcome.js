8f738dfe3c4089de6d32f4353f5127778f847893
Marko <marko.k.89@gmail.com>
2018-01-16 13:08:38 +0100

    styled login and started styling welcome

--- src/auth/welcome.js
+++ src/auth/welcome.js
@@ -4,7 +4,13 @@ export default function (props) {
     // console.log('ucitaj')
     return (
         <div id="welcome">
-            <h3>Welcome to Lab Notebook...we need a catchy name</h3>
+
+            <figure>
+                <img src="/public/images/flaskIcon.png" alt="DreamLab NB log pink flask" />
+            </figure>
+
+            <h3>CloudNotebook</h3>
+
             {
                 // shows property of chhildren components
             }
